f901c173668a753e2df1021d7c3e877da43c455e
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-06-18 16:45:55 +0300

    init commit

---
+++ src/test/java/ru/stepagin/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepagin.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
