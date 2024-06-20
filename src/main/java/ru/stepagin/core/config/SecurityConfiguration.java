b2c38d07472c5f0cb12d622b46071b64288a91a6
Stepan <77981177+Maskvizy@users.noreply.github.com>
2024-06-20 20:46:16 +0300

    migrate to PostgreSQL

--- src/main/java/ru/stepagin/core/config/SecurityConfiguration.java
+++ src/main/java/ru/stepagin/core/config/SecurityConfiguration.java
@@ -50,7 +50,6 @@ public class SecurityConfiguration {
                     registry.requestMatchers("/api-docs").permitAll();
                     registry.requestMatchers("/swagger-ui.html").permitAll();
                     registry.requestMatchers("/swagger-ui/index.html").permitAll();
-                    registry.requestMatchers("/h2-console/**").permitAll();
                     registry.requestMatchers("/favicon.ico").permitAll();
                     registry.requestMatchers(baseUrl + "/auth/registration").permitAll();
 
